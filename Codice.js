1b4dd5b250f5ba80651d9d054dd4ccd32c6cdc3d
Luca Giuberti <gix_lg@yahoo.com>
2019-02-03 17:04:27 +0100

    Step 1 - Label the mail

--- Codice.js
+++ Codice.js
@@ -1,18 +1,39 @@
-function printMessagesInfo(messages){
-  messages.forEach(function(message){
-    var from = message.getFrom();
-    var date = message.getDate();
-    var subject = message.getSubject();
-    
-    Logger.log('Message subject: "%s" from: %s  date:%s', subject, from, date);
-  });
+function getYearAndMonthStringFromDate(date){  
+  if (date.getMonth() == 11) {    //I do a  month +1 because e.g the fee on January should be paid in February
+    var nextMonth = new Date(date.getFullYear() + 1, 0, 1);
+  } else {
+    var nextMonth = new Date(date.getFullYear(), date.getMonth() + 1, 1, date.getHours(), date.getMinutes(), 0, 0);
+  }
+  
+  var month = nextMonth.getUTCMonth() + 1; //months from 1-12
+  var day = nextMonth.getUTCDate();
+  var year = nextMonth.getUTCFullYear();
+  
+  var stringYear = "" + year;
+  var stringMonth = month<10 ? '0'+month : "";
+  return stringYear + "-" + stringMonth;
+}
+
+function processesThread(thread){
+  var messages = GmailApp.getMessagesForThread(thread);
+  var message = messages[0];      //In my scenario normally I have only one message for thread for that kind of mail
+  var newLabel = "Casa Gorgonzola/"+getYearAndMonthStringFromDate(message.getDate());
+  thread.addLabel(GmailApp.createLabel(newLabel)); 
+}
+
+function isThreadAlreadyProcessed(thread){
+  return thread.getLabels().some(function(label){ return label.getName() == "Processed by Google Script" });
 }
 
 function main() {
   var label = GmailApp.getUserLabelByName("Need to be processed");
   var threads = label.getThreads();
   for (var i = threads.length - 1; i >= 0; i--) { //Process them in the order received
-    var thread = threads[i];    
-    printMessagesInfo(GmailApp.getMessagesForThread(thread));
-  } 
+    var thread = threads[i];   
+    
+    if (!isThreadAlreadyProcessed(thread)){    
+      processesThread(thread);
+      thread.addLabel(GmailApp.getUserLabelByName("Processed by Google Script"));
+    }
+  }  
 }
