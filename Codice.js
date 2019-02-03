0b0063ffcc1d6e466a202f8bde0e51631e9c28e3
Luca Giuberti <gix_lg@yahoo.com>
2019-02-03 16:05:41 +0100

    Step 0 - Read mail and log data

--- Codice.js
+++ Codice.js
@@ -1,3 +1,18 @@
+function printMessagesInfo(messages){
+  messages.forEach(function(message){
+    var from = message.getFrom();
+    var date = message.getDate();
+    var subject = message.getSubject();
+    
+    Logger.log('Message subject: "%s" from: %s  date:%s', subject, from, date);
+  });
+}
+
 function main() {
-  return "Hi";
+  var label = GmailApp.getUserLabelByName("Need to be processed");
+  var threads = label.getThreads();
+  for (var i = threads.length - 1; i >= 0; i--) { //Process them in the order received
+    var thread = threads[i];    
+    printMessagesInfo(GmailApp.getMessagesForThread(thread));
+  } 
 }
